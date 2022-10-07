1f9cac40a2d4895822c28e57459500faad8ab879
WhoAteDaCake <doitsimplefy@gmail.com>
2022-10-07 09:31:49 +0100

    code

--- Program.fs
+++ Program.fs
@@ -7,9 +7,10 @@ let main args =
     let baseDir = Directory.GetCurrentDirectory ()
     let myDir =
         if baseDir.Contains debugLocation then
-            baseDir.Replace(debugLocation, "")
+            __SOURCE_DIRECTORY__
         else
             baseDir
     let config = Configuration.load(Path.Join(myDir, "examples/configuration.yml"))
+    let compose = Compose.load(Path.Join(myDir, "examples/docker-compose.yml"))
     // Return 0. This indicates success.
     0
